a3a58d8b12079a18a36bcd329fef9cd278f2b7df
EricNguyen0 <e1446191@cmaisonneuve.qc.ca>
2024-04-18 12:05:07 -0400

    s10c2 carrousel

--- js/carrousel.js
+++ js/carrousel.js
@@ -1,35 +1,52 @@
 (function(){
-  console.log("Hello World!")
+  console.log("Vive Javascript")
   let carrousel = document.querySelector('.carrousel')
+  console.log("conteneur carrousel = " + carrousel.tagName)
   let bouton = document.querySelector('.bouton__ouvrir')
+  console.log("bouton = " + bouton.tagName)
   let carrousel__x = document.querySelector('.carrousel__x')
+  console.log('carrousel__x' + carrousel__x.tagName)
   let galerie = document.querySelector('.galerie')
+  console.log('galerie = ' + galerie.tagName)
+
+
   let carrousel__figure = document.querySelector('.carrousel__figure')
-  // Création dynamique d'une image du carrousel
+  /* Création dynamique d'une image du carrousel */
   let carrousel__img = document.createElement('img')
   carrousel__img.classList.add('carrousel__img')
-  let galerie__img = galerie.querySelector('img')
-  console.log(galerie__img)
-  
+  /* récupère la première image de la galerie */
+  // let galerie__img = galerie.querySelector('img')
+  /* por créer une collection d'images de la galerie */
+  let galerie__img = galerie.querySelectorAll('img')
+  console.log( galerie__img)
+  for (const elm of galerie__img)
+  {
+      console.log(elm.src)
+  }
+
+  /*
+  console.log("première image de la galerie = " + galerie__img.src)
   carrousel__img.src = galerie__img.src
+  console.log("première image du carrousel = " + carrousel__img.src)
   carrousel__figure.appendChild(carrousel__img)
+  console.log(carrousel__figure)
+*/
 
 
-  let carrousel_img = document.createElement('img')
-  carrousel_img.classList.add('carrousel__img')
 
-  console.log('carrousel__x' + carrousel__x.tagName)
 
-  // Écouteur d'événement sur le bouton ouvrir
+
+
+
+/* écouteur pour ouvrir la boîte modale */
   bouton.addEventListener('mousedown', function(){
-    carrousel.classList.add('carrousel--ouvrir')
+      carrousel.classList.add('carrousel--ouvrir') // ouvrir le carrousel
   })
-
-  // Écouteur d'événement sur le bouton fermer
+/* Écouteur pour fermer la boîte modale */
   carrousel__x.addEventListener('mousedown', function(){
-    carrousel.classList.remove('carrousel--ouvrir')
+      carrousel.classList.remove('carrousel--ouvrir') // fermer le carrousel
   })
 
 
-  
+
 })()
